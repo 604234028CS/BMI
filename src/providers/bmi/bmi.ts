b4d0a963e609e61f8604664dc1e8a37335f637cd
Adeenas <604234028@parichat.skru.ac.th>
2018-12-25 11:48:33 +0700

    UpdateBMI

---
+++ src/providers/bmi/bmi.ts
@@ -0,0 +1,11 @@
+
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class BmiProvider {
+
+  bmiCalculate(height:number,weight:number){
+    return (weight / height) / height;
+  }
+
+}
